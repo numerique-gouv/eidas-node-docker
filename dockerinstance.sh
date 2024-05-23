4954dae514d030092a098877166e822a1c9a0d61
Aurélien Merel <aurelien.merel@beta.gouv.fr>
2024-05-23 17:42:00 +0200

    dockerinstance.sh : prise en compte version

--- dockerinstance.sh
+++ dockerinstance.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-if [ -z "$EIDAS_INSTANCE" -o -z "$EIDAS_PORT" ]; then
-  echo Define EIDAS_INSTANCE and EIDAS_PORT
+if [ -z "$EIDAS_INSTANCE" -o -z "$EIDAS_PORT" -o -z "$EIDAS_VERSION" ]; then
+  echo Define EIDAS_INSTANCE, EIDAS_PORT and EIDAS_VERSION
   exit 1
 fi
 
@@ -17,4 +17,4 @@ ls -d /etc/eidas/instances/$EIDAS_INSTANCE
 docker run \
  -p '[::1]':$EIDAS_PORT:8080 \
  --mount type=bind,source=/etc/eidas/instances/$EIDAS_INSTANCE,target=/config/eidas,readonly \
- eidas-$EIDAS_TYPE:tomcat-latest
+ eidas-${EIDAS_TYPE}-${EIDAS_VERSION}:tomcat-latest
