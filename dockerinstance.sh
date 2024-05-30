a8c637a93c4ca04cdb26019761ea93abc9019391
Aurélien Merel <aurelien.merel@beta.gouv.fr>
2024-05-30 16:36:40 +0200

    dockerinstance.sh : port ignite

--- dockerinstance.sh
+++ dockerinstance.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-if [ -z "$EIDAS_INSTANCE" -o -z "$EIDAS_PORT" -o -z "$EIDAS_VERSION" ]; then
-  echo Define EIDAS_INSTANCE, EIDAS_PORT and EIDAS_VERSION
+if [ -z "$EIDAS_INSTANCE" -o -z "$EIDAS_PORT" -o -z "$EIDAS_VERSION" -o -z "$IGNITE_PORT" ]; then
+  echo Define EIDAS_INSTANCE, EIDAS_PORT, EIDAS_VERSION and IGNITE_PORT
   exit 1
 fi
 
@@ -16,5 +16,6 @@ ls -d /etc/eidas/instances/$EIDAS_INSTANCE
 
 docker run \
  -p '[::1]':$EIDAS_PORT:8080 \
+ -p $IGNITE_PORT:10900 \
  --mount type=bind,source=/etc/eidas/instances/$EIDAS_INSTANCE,target=/config/eidas,readonly \
  eidas-${EIDAS_TYPE}-${EIDAS_VERSION}:tomcat-latest
